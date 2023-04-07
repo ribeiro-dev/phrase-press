fc6c3a014ac64d4a558d3facb90770230a43f523
ribeiro-dev <romilson.2r@gmail.com>
2023-04-07 20:08:21 -0300

    add: tables relationship

--- articles/Article.js
+++ articles/Article.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize")
 const connection = require("../database/database")
+const Category = require('../categories/Category')
 
 const Article = connection.define('articles', {
     title: {
@@ -15,5 +16,9 @@ const Article = connection.define('articles', {
         allowNull: false
     }
 })
+
+// creates the relationship
+Category.hasMany(Article) // one-to-many
+Article.belongsTo(Category) // one-to-one
 
 module.exports = Article
