c22017887db3efdad2ca87b86ba074e43695de56
ribeiro-dev <romilson.2r@gmail.com>
2023-04-09 17:46:38 -0300

    add: page to manage categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -4,6 +4,14 @@ const slugify = require('slugify')
 
 const Category = require('./Category')
 
+
+router.get('/admin/categories', (req, res) => {
+
+    Category.findAll().then(categories => {
+        res.render('admin/categories/index', { categories: categories })
+    })
+})
+
 router.get('/admin/categories/new', (req, res) => {
     res.render('admin/categories/new')
 });
@@ -24,7 +32,22 @@ router.post('/categories/save', (req, res) => {
     .then(() => {
         res.redirect('/')
     })
+})
 
+router.post('/categories/delete', (req, res) => {
+    const categoryId = req.body.id
+
+    if (categoryId == undefined) res.redirect('/admin/categories')
+    if (isNaN(categoryId)) res.redirect('/admin/categories')
+
+    Category.destroy({
+        where: {
+            id: categoryId
+        }
+    })
+    .then(() => {
+        res.redirect('/admin/categories')
+    })
 })
 
 module.exports = router
