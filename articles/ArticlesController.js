3ede18916943a27102ee1dc2e743201c3e33b534
ribeiro-dev <romilson.2r@gmail.com>
2023-04-11 23:55:50 -0300

    add: new article form

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -6,7 +6,7 @@ router.get('/articles', (req, res) => {
 });
 
 router.get('/admin/articles/new', (req, res) => {
-    res.send("ROUTE TO CREATE NEW ARTICLE")
+    res.render("admin/articles/new")
 })
 
 module.exports = router
