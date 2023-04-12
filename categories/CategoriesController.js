e4d029bdde0fcbadcbdd039a51efccb8bb95b45d
ribeiro-dev <romilson.2r@gmail.com>
2023-04-11 23:44:40 -0300

    add: functionality to edit categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -66,4 +66,22 @@ router.get('/admin/categories/edit/:id', (req, res) => {
         })
 })
 
+router.post('/categories/update', (req, res) => {
+    const id = req.body.id
+    const title = req.body.title
+
+    Category.update({
+        title: title,
+        slug: slugify(title, {
+            lower: true
+        })
+    }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect('/admin/categories')
+    })
+})
+
 module.exports = router
