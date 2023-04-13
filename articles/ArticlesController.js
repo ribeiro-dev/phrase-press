42634f74353ff9b30e1b8859d0168920b9a46c97
ribeiro-dev <romilson.2r@gmail.com>
2023-04-13 00:33:25 -0300

    add: create article functionality

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router() // create router instance
-const Category = require('../categories/Category')
+const Category = require('../categories/Category');
+const Article = require('./Article')
+const slugify = require('slugify');
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
     res.send("ARTICLES ROUTE")
 });
 
@@ -10,6 +12,24 @@ router.get('/admin/articles/new', (req, res) => {
     Category.findAll().then(categories => {
             res.render("admin/articles/new", { categories: categories})
         })
+})
+
+router.post('/articles/save', (req, res) => {
+    const title = req.body.title
+    const category = req.body.category
+    const body = req.body.body
+
+    Article.create({
+        title: title,
+        body: body,
+        slug: slugify(title, {
+            lower: true
+        }),
+        categoryId: category
+    })
+    .then(() => {
+        res.redirect('/admin/articles')
+    })
 })
 
 module.exports = router
