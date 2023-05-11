239d12127d635caaeb60dc41f3af3cfec2bbff5f
ribeiro-dev <romilson.2r@gmail.com>
2023-05-10 23:28:51 -0300

    add: user creation / password hashing

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const User = require("./User")
+const bcrypt = require("bcryptjs")
 
 router.get("/admin/users", (req, res) => {
     res.send("Users List")
@@ -8,6 +9,23 @@ router.get("/admin/users", (req, res) => {
 
 router.get("/admin/users/create", (req, res) => {
     res.render("admin/users/create")
+})
+
+router.post("/users/create", (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.redirect('/admin/users')
+    }).catch(err => {
+        res.redirect('/')
+    })
 })
 
 module.exports = router
